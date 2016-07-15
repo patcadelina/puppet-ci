09d6ce4b6b37317bcde9067141bb1197af9b5291
Patrick Cadelina <patrick.cadelina@freelancer.com>
2016-07-15 17:51:34 +0800

    Fix reverse proxy

--- modules/ci/manifests/init.pp
+++ modules/ci/manifests/init.pp
@@ -3,7 +3,10 @@ class ci {
   include jenkins
 
   nginx::resource::vhost { 'default':
-    listen_port => 80,
-    proxy       => 'http://127.0.0.1:8080',
+    listen_port      => 80,
+    proxy            => 'http://127.0.0.1:8080',
+    proxy_set_header => [
+      'X-Forwarded-Proto $scheme'
+    ],
   }
 }
